2056aa6328933c3df1105aec9d7fd6e98f277fef
htnabe <n21m312@matsu.shimane-u.ac.jp>
2022-03-31 09:46:09 +0900

    update lects info

--- screens/lectureScreens/searchResultScreen/loadFirstSemisterFile.js
+++ screens/lectureScreens/searchResultScreen/loadFirstSemisterFile.js
@@ -10,40 +10,40 @@ function loadFirstSemisterFile(fileName) {
     // 文字列を動的に変化させてrequireすることは不可能なので仕方なくswitch文
     switch (fileName) {
       case '総合理工':
-        data = require('./FirstSemisterLecs/総合理工.json');
+        data = require('./firstSemiLects/総合理工.json');
         break;
       case '教養教育':
-        data = require('./FirstSemisterLecs/教養教育.json');
+        data = require('./firstSemiLects/教養教育.json');
         break;
       case '生物資源':
-        data = require('./FirstSemisterLecs/生物資源.json');
+        data = require('./firstSemiLects/生物資源.json');
         break;
       case '人間科学':
-        data = require('./FirstSemisterLecs/人間科学.json');
+        data = require('./firstSemiLects/人間科学.json');
         break;
       case '人間社会科学':
-        data = require('./FirstSemisterLecs/人間社会科学.json');
+        data = require('./firstSemiLects/人間社会科学.json');
         break;
       case '教育':
-        data = require('./FirstSemisterLecs/教育.json');
+        data = require('./firstSemiLects/教育.json');
         break;
       case '教育学':
-        data = require('./FirstSemisterLecs/教育学.json');
+        data = require('./firstSemiLects/教育学.json');
         break;
       case '法文':
-        data = require('./FirstSemisterLecs/法文.json');
+        data = require('./firstSemiLects/法文.json');
         break;
       case '人文科学':
-        data = require('./FirstSemisterLecs/人文科学.json');
+        data = require('./firstSemiLects/人文科学.json');
         break;
       case '教育学_教職':
-        data = require('./FirstSemisterLecs/教育学（教職）.json');
+        data = require('./firstSemiLects/教育学(教職).json');
         break;
       case '自然科学':
-        data = require('./FirstSemisterLecs/自然科学.json');
+        data = require('./firstSemiLects/自然科学.json');
         break;
       case '総合理工_博士後期':
-        data = require('./FirstSemisterLecs/総合理工（博士後期）.json');
+        data = require('./firstSemiLects/総合理工(博士後期).json');
         break;
       default:
         break;
--- screens/lectureScreens/searchResultScreen/loadSecondSemisterFile.js
+++ screens/lectureScreens/searchResultScreen/loadSecondSemisterFile.js
@@ -17,63 +17,63 @@ async function loadSecondSemisterFile(fileName) {
     // 文字列を動的に変化させてrequireすることは不可能なので仕方なくswitch文
     switch (fileName) {
       case '総合理工':
-        yearAroundData = require('./FirstSemisterLecs/総合理工.json');
-        data = require('./SecondSemisterLecs/総合理工.json');
+        yearAroundData = require('./firstSemiLects/総合理工.json');
+        data = require('./secondSemiLects/総合理工.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '教養教育':
-        yearAroundData = require('./FirstSemisterLecs/教養教育.json');
-        data = require('./SecondSemisterLecs/教養教育.json');
+        yearAroundData = require('./firstSemiLects/教養教育.json');
+        data = require('./secondSemiLects/教養教育.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '生物資源':
-        yearAroundData = require('./FirstSemisterLecs/生物資源.json');
-        data = require('./SecondSemisterLecs/生物資源.json');
+        yearAroundData = require('./firstSemiLects/生物資源.json');
+        data = require('./secondSemiLects/生物資源.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '人間科学':
-        yearAroundData = require('./FirstSemisterLecs/人間科学.json');
-        data = require('./SecondSemisterLecs/人間科学.json');
+        yearAroundData = require('./firstSemiLects/人間科学.json');
+        data = require('./secondSemiLects/人間科学.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '人間社会科学':
-        yearAroundData = require('./FirstSemisterLecs/人間社会科学.json');
-        data = require('./SecondSemisterLecs/人間社会科学.json');
+        yearAroundData = require('./firstSemiLects/人間社会科学.json');
+        data = require('./secondSemiLects/人間社会科学.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '教育':
-        yearAroundData = require('./FirstSemisterLecs/教育.json');
-        data = require('./SecondSemisterLecs/教育.json');
+        yearAroundData = require('./firstSemiLects/教育.json');
+        data = require('./secondSemiLects/教育.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '教育学':
-        yearAroundData = require('./FirstSemisterLecs/教育学.json');
-        data = require('./SecondSemisterLecs/教育学.json');
+        yearAroundData = require('./firstSemiLects/教育学.json');
+        data = require('./secondSemiLects/教育学.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '法文':
-        yearAroundData = require('./FirstSemisterLecs/法文.json');
-        data = require('./SecondSemisterLecs/法文.json');
+        yearAroundData = require('./firstSemiLects/法文.json');
+        data = require('./secondSemiLects/法文.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '人文科学':
-        yearAroundData = require('./FirstSemisterLecs/人文科学.json');
-        data = require('./SecondSemisterLecs/人文科学.json');
+        yearAroundData = require('./firstSemiLects/人文科学.json');
+        data = require('./secondSemiLects/人文科学.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '教育学_教職':
-        yearAroundData = require('./FirstSemisterLecs/教育学（教職）.json');
-        data = require('./SecondSemisterLecs/教育学（教職）.json');
+        yearAroundData = require('./firstSemiLects/教育学(教職).json');
+        data = require('./secondSemiLects/教育学（教職）.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '自然科学':
-        yearAroundData = require('./FirstSemisterLecs/自然科学.json');
-        data = require('./SecondSemisterLecs/自然科学.json');
+        yearAroundData = require('./firstSemiLects/自然科学.json');
+        data = require('./secondSemiLects/自然科学.json');
         data = await filterYearData(yearAroundData, data);
         break;
       case '総合理工_博士後期':
-        yearAroundData = require('./FirstSemisterLecs/総合理工（博士後期）.json');
-        data = require('./SecondSemisterLecs/総合理工（博士後期）.json');
+        yearAroundData = require('./firstSemiLects/総合理工(博士後期).json');
+        data = require('./secondSemiLects/総合理工（博士後期）.json');
         data = await filterYearData(yearAroundData, data);
         break;
       default:
