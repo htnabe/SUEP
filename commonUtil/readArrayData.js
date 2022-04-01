78c2980e81104aaff76695f7ebb571c374e93dab
htnabe <n21m312@matsu.shimane-u.ac.jp>
2022-04-01 11:27:27 +0900

    remove console codes

--- commonUtil/readArrayData.js
+++ commonUtil/readArrayData.js
@@ -6,10 +6,8 @@ const readArrayData = async (key) => {
   try {
     const stringValue = await AsyncStorage.getItem(key);
     if (stringValue != null) {
-      console.log('キー ' + key + ' は値を持っています。' + '\n');
       return JSON.parse(stringValue);
     } else {
-      console.log('キー ' + key + ' は値を持っていません。' + '\n');
       return null;
     }
   } catch (error) {
